6287b169c81f75759957d7ca3b0ed1f22e513c9a
Filipe Campos <filipepcampos24@gmail.com>
2022-09-20 18:40:54 +0100

    Finish creating SQLite database

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -3,3 +3,8 @@
 .separator ;
 .import --skip 1 data/account.csv account
 .import --skip 1 data/client.csv client
+.import --skip 1 data/card_dev.csv cardDev
+.import --skip 1 data/disp.csv disp
+.import --skip 1 data/district.csv district
+.import --skip 1 data/loan_dev.csv loanDev
+.import --skip 1 data/trans_dev.csv transDev
