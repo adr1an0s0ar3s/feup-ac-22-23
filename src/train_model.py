fff111d19ffd3e738e49649ae696911aeb4c4ebc
Filipe Campos <filipepcampos24@gmail.com>
2022-10-02 12:02:34 +0100

    Use params instead of metrics in train

--- src/train_model.py
+++ src/train_model.py
@@ -2,13 +2,17 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from joblib import dump
 import json
+import dvc.api
 
 def main():
     # Load train data
     X_train, y_train = pd.read_csv('data/X_train.csv'), pd.read_csv('data/y_train.csv')
 
+    # Load parameters
+    params = dvc.api.params_show()['train']
+
     # Create model
-    model = DecisionTreeClassifier(max_depth=5)
+    model = DecisionTreeClassifier(max_depth=params['max_depth'])
 
     # Train model
     model.fit(X_train, y_train)
