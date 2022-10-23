c30cd96c07008ae2e2f97966b8615e71104dc569
adr1an0s0ar3s <adrianosoares35@gmail.com>
2022-10-23 18:03:07 +0100

    feat: add unify_data.sql query to unify_data.py

--- src/unify_data.py
+++ src/unify_data.py
@@ -3,8 +3,15 @@ import pandas as pd
 
 def main():
     con = sqlite3.connect('data/database.db')
-    df = pd.read_sql_query('SELECT * FROM loanDev', con)
-    df = df.drop('id', axis=1)
+
+    df = pd.read_sql_query('SELECT ownerSex, ownerBirthday, ownerCardType, frequency, account_view.date AS accountCreationDate, isShared, balance, loanDev_view.date AS loanDate, duration as loanDuration, payments as loanPayments, amount as loanAmount, ratio, districtName, region, nInhabitants, nMunicipalitiesSub499Inhabitants, nMunicipalities500to1999Inhabitants, nMunicipalities2000to9999Inhabitants, nMunicipalitiesOver10000Inhabitants, nCities, urbanInhabitantsRatio, averageSalary, unemploymentRate95, unemploymentRate96, nEnterpreneursPer1000Inhabitants, commitedCrimes95, commitedCrimes96, status \
+        FROM loanDev_view \
+        JOIN account_view ON account_view.id = loanDev_view.accountId \
+        JOIN owners_sex_birthday_view ON owners_sex_birthday_view.accountId = account_view.id \
+        JOIN owners_card_type ON owners_card_type.accountId = account_view.id \
+        JOIN district_view ON district_view.id = account_view.districtId'
+    , con)
+
     df.to_csv('data/unified_data.csv', index=False)
 
 if  __name__ == '__main__':
