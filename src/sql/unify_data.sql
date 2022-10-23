ef5a9f57fba461a5ab186aa1cabd4c4562a0b94a
adr1an0s0ar3s <adrianosoares35@gmail.com>
2022-10-23 17:57:20 +0100

    feat: add district_view join to unify_data.sql

--- src/sql/unify_data.sql
+++ src/sql/unify_data.sql
@@ -9,10 +9,11 @@
 -- all district info about owner
 -- latestTransDate, accountBalance, transVolume, sumAllTransAmount (intake-outake)
 
--- To add: district and transDev
-SELECT ownerSex, ownerBirthday, ownerCardType, frequency, account_view.date AS accountCreationDate, isShared, balance, loanDev_view.date AS loanDate, duration as loanDuration, payments as loanPayments, amount as loanAmount, ratio, status
+-- To add: and transDev
+SELECT ownerSex, ownerBirthday, ownerCardType, frequency, account_view.date AS accountCreationDate, isShared, balance, loanDev_view.date AS loanDate, duration as loanDuration, payments as loanPayments, amount as loanAmount, ratio, districtName, region, nInhabitants, nMunicipalitiesSub499Inhabitants, nMunicipalities500to1999Inhabitants, nMunicipalities2000to9999Inhabitants, nMunicipalitiesOver10000Inhabitants, nCities, urbanInhabitantsRatio, averageSalary, unemploymentRate95, unemploymentRate96, nEnterpreneursPer1000Inhabitants, commitedCrimes95, commitedCrimes96, status
 FROM loanDev_view
 JOIN account_view ON account_view.id = loanDev_view.accountId
 JOIN owners_sex_birthday_view ON owners_sex_birthday_view.accountId = account_view.id
 JOIN owners_card_type ON owners_card_type.accountId = account_view.id
+JOIN district_view ON district_view.id = account_view.districtId
 LIMIT(5);
