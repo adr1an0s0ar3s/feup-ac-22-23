874f71271559ee137e1b71a51aee6bd6c5bc6372
adr1an0s0ar3s <adrianosoares35@gmail.com>
2022-11-04 16:33:40 +0000

    feat: calculate commitedCrimes95 and unemploymentRate95 missing values with mean difference between 95 and 96, update missing values notebook

--- src/data-cleaning/missing_data.py
+++ src/data-cleaning/missing_data.py
@@ -4,13 +4,16 @@ def main():
     df = pd.read_csv('data/clean_data0.csv')
 
     cc95 = pd.to_numeric(df['commitedCrimes95'], errors='coerce')
-    cc95[cc95.isna()] = df[cc95.isna()]['commitedCrimes96']
+    diff = df['commitedCrimes96'] - cc95
+    averageDiff = diff.mean()
+    cc95[cc95.isna()] = df['commitedCrimes96'][cc95.isna()] - averageDiff
     df['commitedCrimes95'] = cc95.astype(int)
 
-    df['unemploymentRate96'] = df['unemploymentRate96'].astype(int)
     ur95 = pd.to_numeric(df['unemploymentRate95'], errors='coerce')
-    ur95[ur95.isna()] = df[ur95.isna()]['unemploymentRate96']
-    df['unemploymentRate95'] = ur95.astype(int)
+    diff = df['unemploymentRate96'] - ur95
+    averageDiff = diff.mean()
+    ur95[ur95.isna()] = df['unemploymentRate96'][ur95.isna()] - averageDiff
+    df['unemploymentRate95'] = ur95
 
     df['hasCard'] = df['ownerCardType'].apply(lambda x: False if pd.isna(x) else True)
     df = df.drop('ownerCardType', axis=1)
